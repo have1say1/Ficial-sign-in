7da98977fc9a955b01f64a495ae9a2cfcb629939
Darkone0 <jialc@pku.edu.cn>
2019-12-23 16:56:12 +0800

    add ficial recognize function

---
+++ Facialsignin/app/src/main/java/com/example/air/facial_sign_in/activities/TestActivity.java
@@ -0,0 +1,15 @@
+package com.example.air.facial_sign_in.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.air.facial_sign_in.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
---
+++ Facialsignin/app/src/main/java/com/example/air/facial_sign_in/util/face/RequestLivenessStatus.java
@@ -0,0 +1,5 @@
+package com.example.air.facial_sign_in.util.face;
+
+public class RequestLivenessStatus {
+    public static final int ANALYZING = 10;
+}
